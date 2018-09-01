1681c2f78aba0956fb3309de776a51e265cb9111
github username <github email address>
2018-08-31 22:26:37 -0400

    pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_07_14_210824) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "jobs", force: :cascade do |t|
     t.string "title"
     t.string "company_name"
